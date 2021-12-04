0673af079d08267628ec391b5e7e5f6098849bce
sosso21 <sofianetop21.st@gmail.com>
2021-12-04 05:56:01 +0100

    03-12-2

---
+++ build/precache-manifest.2e75ad3e36d32929d245e759ec9bdbbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610fecbb2c568c565eda4c1c897edb29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ab048a1db010d80f7c",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "2b2ca0f05874c61d4ec2",
+    "url": "/static/js/2.a2d2041e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.a2d2041e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ab048a1db010d80f7c",
+    "url": "/static/js/main.de96ef0e.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "1979aad3a6f99cdcc4e41d7c462d0cec",
+    "url": "/static/media/logo.1979aad3.png"
+  }
+]);
